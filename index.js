b09af5c0bc6fc372e4d8eba1686884a31e304c9d
Osorabrian <osorabrian@gmail.com>
2022-12-07 20:56:17 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const splittedWord = word.split("")
+  const reversedArray = splittedWord.reverse()
+  const reversedWord = reversedArray.join("")
+  
+  if(word.toLowerCase() === reversedWord.toLowerCase()){
+    return true
+  }else{
+    return false
+  }
+
 }
 
 /* 
-  Add your pseudocode here
+  Split the word into list
+  reverse the list
+  join the list
+  if condtion to check pallindrome 
 */
 
 /*
-  Add written explanation of your solution here
+  The function will take in a string, then split into a list then we reverse the list 
+  after the list has been reversed we join it to form a reversed string. After that we compare
+  if the original string and the reversed string match using if condition, if they are equal
+  we should return true else we return false.
 */
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
